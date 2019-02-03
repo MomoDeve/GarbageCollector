8fd871eedb18a584a67f35b3adf27d65de41edf3
#Momo <40578274+MomoDeve@users.noreply.github.com>
2019-02-03 20:25:36 +0300

    Update collector_test.c

--- collector_test.c
+++ collector_test.c
@@ -2,26 +2,32 @@
 #include <stdlib.h>
 #include "gc.h"
 
-//creating 1024 elements in stack, making all their parents unmarked (except gc_root) in a cycle
+static GC* gc;
+
+typedef struct
+{
+	int first, second;
+} int_pair;
+
+int_pair* pair_sum(object* o, int_pair* p1, int_pair* p2)
+{
+	int_pair* res = new(int_pair, o, gc);
+	res->first = p1->first + p2->first;
+	res->second = p1->second + p2->second;
+	return res;
+}
 
 int main()
 {
-	object* o  = (object*)malloc(sizeof(object));
-	o->marked = 0;
-	o->parent = NULL;
-	GC* gc = gc_init();
-	for (int i = 1; i < 1024; i++)
-		gc_push(gc, gc_root(gc));
-	for(unsigned int i = 0; ; i++)
-	{
-		printf("cycle: %u\n", i);
-		for (int i = 1; i < 1024; i++)
-			gc->stack[i]->parent = o;
-		for (int i = 1; i < 1024; i++)
-			gc_push(gc, gc_root(gc));
-	}
+	gc = gc_init();
+	_push(gc);
+	int_pair* p1 = new(int_pair, _cur(gc), gc);
+	p1->first = 3, p1->second = 4;
+	int_pair* p2 = new(int_pair, _cur(gc), gc);
+	p2->first = 7, p2->second = -14;
+	int_pair* p3 = pair_sum(_cur(gc), p1, p2);
+	printf("sum of pairs: {%d, %d}\n", p3->first, p3->second);
+	_pop(gc);
 	gc_free(gc);
-	free(o);
-	printf("success\n");
 	system("pause");
 }
