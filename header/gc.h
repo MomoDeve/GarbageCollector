1dcc2ddb5a4bf5c0af1d50102df7cd679edeab9a
#Momo <40578274+MomoDeve@users.noreply.github.com>
2019-02-03 19:55:18 +0300

    added macros for easy-object add and alloc

--- header/gc.h
+++ header/gc.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifndef bool
-typedef unsigned int bool;
+typedef unsigned short int bool;
 #endif
 
 #define STACKSIZE 1024
@@ -22,16 +22,22 @@ typedef struct
 
 GC* gc_init();
 
-void gc_free(GC*);
+void gc_free(GC* gc);
 
-object* gc_root(GC*);
+object* gc_root(GC* gc);
 
-object* gc_push(GC*, object*);
+object* gc_push(GC* gc, object* parent);
 
-void gc_mark_objects(GC*);
+#define new_o(parent, gc) (gc_push(gc, parent))
 
-void gc_collect_garbage(GC*);
+#define alloc(object_type) (malloc(sizeof(object_type)))
 
-void gc_realloc(GC*);
+#define new(object_type, parent, gc) ((object_type*)(gc_push(gc, parent)->data = malloc(sizeof(object_type))))
 
-//TODO
+#define _push(gc) object* __gc_object = gc_push(gc, gc_root(gc)); __gc_object->marked = 1
+
+#define _cur(gc) (__gc_object) 
+
+#define _last(gc) _cur(gc)
+
+#define _pop(gc) __gc_object->parent = gc_push(gc, __gc_object);
